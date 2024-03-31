c6c24b3d0d9797de483e30fc6211e7f2e3d4b36a
mlkommer <m.kommer@mid.de>
2024-03-31 16:37:33 +0200

    Update blockchain.py
    Hash function

--- Blockchain/blockchain.py
+++ Blockchain/blockchain.py
@@ -33,5 +33,9 @@ class Blockchain:
                 new_proof += 1
         return new_proof
 
+    def hash(self, block):
+        encoded_block = json.dumps(block, sort_keys = True).encode()
+        return hashlib.sha256(encoded_block).hexdigest()
+
 # Mining the Blockchain
 
