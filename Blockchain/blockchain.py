3ab0aee342676374b97f534d3ec8a526515b726d
mlkommer <m.kommer@mid.de>
2024-03-31 16:20:53 +0200

    Create blockchain.py
    Blockchain init

---
+++ Blockchain/blockchain.py
@@ -0,0 +1,15 @@
+# Import the libs
+import datetime
+import hashlib
+import json
+from flask import Flask, jsonify
+
+# Building the Blockchain
+class Blockchain:
+
+    def __init__(self):
+        self.chain = []
+        self.create_block(proof = 1, previous_hash = '0')
+
+# Mining the Blockchain
+
