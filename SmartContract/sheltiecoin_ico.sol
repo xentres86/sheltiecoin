19245ee43932690eeb80aa4b555f7ab0221d3268
mlkommer <m.kommer@mid.de>
2024-04-04 14:04:11 +0200

    Update sheltiecoin_ico.sol
    Vars for max sheltiecoins, conversion rate and invested coins added

--- SmartContract/sheltiecoin_ico.sol
+++ SmartContract/sheltiecoin_ico.sol
@@ -1,1 +1,18 @@
+// SPDX-License-Identifier: MIT 
 // Sheltiecoin ICO
+
+// Version of Solidity compiler
+pragma solidity ^0.8.19;
+
+contract sheltiecoin_ico {
+
+    // Introducing the maximum number of Sheltiecoins available for sale
+    uint public max_sheltiecoins = 1000000;
+
+    // Introducing the USD to Sheltiecoins conversion rate
+    uint public usd_to_sheltiecoins = 1000;
+
+    // Introducing the total number of Sheltiecoins that have been bought by the investors
+    uint public total_sheltiecoins_bought = 0;
+
+}
